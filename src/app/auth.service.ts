00196af65f04c6584ce2d4b2972d7380e7de6365
azizbenyaala98 <azizbenyaala@gmail.com>
2023-11-27 19:45:32 +0100

    auth firebase implementation v2

---
+++ src/app/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import firebase from 'firebase/compat/app'
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(public ngFireAuth :AngularFireAuth) { }
+  async registeruser(email:string,password:string){
+    return await this.ngFireAuth.createUserWithEmailAndPassword(email,password)
+  }
+  async loginUser(email:string,password:string){
+    return await this.ngFireAuth.signInWithEmailAndPassword(email,password)
+  }
+  async resetPassword(email:string){
+    return await this.ngFireAuth.sendPasswordResetEmail(email)
+  }
+  async signout(){
+    return await this.ngFireAuth.signOut()
+  }
+async getProfile(){
+  return await this.ngFireAuth.currentUser
+}
+}
